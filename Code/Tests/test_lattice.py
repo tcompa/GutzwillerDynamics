685c60daf6a29a89e7fcc57d795ee9b9e379cd0c
Tommaso Comparin <comparin@lps.ens.fr>
2019-06-24 14:25:05 +0200

    xrange -> range (for py3 compatibility)

--- Code/Tests/test_lattice.py
+++ Code/Tests/test_lattice.py
@@ -11,7 +11,7 @@ def test_lattice_definition_2D():
     xy = numpy.empty(2, dtype=numpy.int32)
     for L in [2, 3, 10, 11]:
         G = Gutzwiller(D=2, L=L)
-        for i_site in xrange(G.N_sites):
+        for i_site in range(G.N_sites):
             x, y = G.site_coords[i_site, :]
             assert i_site == G.xy2i(x, y)
         for x, y in itertools.product(range(L), repeat=2):
@@ -26,8 +26,8 @@ def test_neighbors_reciprocity():
         for D in [1, 2]:
             for L in [5, 10]:
                 G = Gutzwiller(D=D, L=L, OBC=OBC)
-                for i_site in xrange(G.N_sites):
-                    for j_nbr in xrange(G.N_nbr[i_site]):
+                for i_site in range(G.N_sites):
+                    for j_nbr in range(G.N_nbr[i_site]):
                         j_site = G.nbr[i_site, j_nbr]
                         assert i_site in list(G.nbr[j_site, :])
 
